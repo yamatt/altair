4546f337ac05789ca212c847524331225cfd33c5
Dom Rodriguez <shymega@shymega.org.uk>
2024-04-11 18:21:49 +0100

    fix(bot): Make the bot only accept messages from yaMatt
    @yaMatt - this is a fix I've done for your bot. It will now check the
    message (through raw JSON, you may want to add a try/catch for
    `KeywordError`), for your Telegram user ID. I can confirm the ID here is
    yours, feel free to test.
    
    Hopefully this helps!
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/altair/config.py
+++ src/altair/config.py
@@ -3,6 +3,7 @@ import os
 
 class Config:
     TELEGRAM_WEBHOOK_URL: str = os.environ.get("TELEGRAM_WEBHOOK_URL")
+    APPROVED_USER: str = "444943133" # Matt - this is your user ID from Telegram.
     GITHUB_REPO: str = os.environ.get("GITHUB_REPO")
     GITHUB_DEFAULT_BRANCH: str = "main"
     REPO_BLOG_POST_PATH: str = "content/blog"
